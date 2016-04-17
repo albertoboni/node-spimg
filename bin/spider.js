ba5b7af29205ab20252d64f8e8902724a42b4f37
Alberto Boni <betobonix@gmail.com>
2016-04-17 20:30:07 -0300

    testing

--- bin/spider.js
+++ bin/spider.js
@@ -4,11 +4,16 @@ console.log(Spimg);
 
 
 //var url = 'http://www.amazon.com/';
+//var url = 'http://www.independent.co.uk/';
+//var url = 'https://www.flickr.com/explore';
+//var url = 'http://www.focoradical.com.br/todasn1.php?prova=2016.04.10_ironman_703_palmas&tax=precosw68';
+//var url = 'https://www.ativo.com/';
 //var url = 'http://globoesporte.globo.com/';
-var url = 'http://edition.cnn.com/';
+//var url = 'http://edition.cnn.com/';
 //var url = 'http://www.folha.uol.com.br/';
 //var url = 'http://www.integraltrust.com/';
 //var url = 'http://www.metrojornal.com.br/';
+//var url = 'http://imgur.com/';
 
 var spimg = new Spimg(url, {
     max_depth: 1000
